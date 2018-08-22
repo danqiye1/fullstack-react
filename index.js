407cb79c3084fcd3bd804ff36d9c028407b6c684
danqiye1 <yedanqi@gmail.com>
2018-08-22 11:57:07 +0800

    Added OAuth with Google

--- index.js
+++ index.js
@@ -1,12 +1,28 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy ;
+const keys = require('./config/keys');
+
 const app = express();
 
 // 1. Heroku's dynamic port binding
 // Heroku environment variables inject or 5000 if in dev environment
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'there'});
-});
+// Google OAuth strategy using passport
+passport.use(
+    new GoogleStrategy({
+        clientID: keys.googleClientID,
+        clientSecret: keys.googleClientSecret,
+        callbackURL: '/auth/google/callback'
+    }, (accessToken) => {
+        console.log(accessToken);
+    })
+);
+
+// Black magic here: 'google' string is internal identifier for passport to use Google Strategy
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email'] // ask for specific pieces of user's google account
+}));
 
 app.listen(PORT);
